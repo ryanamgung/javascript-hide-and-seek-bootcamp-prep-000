f03b648ec1fc5e9335a8b8af10360505a0ecd180
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-22 04:04:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,33 @@ function nestedTarget()
 
 function increaseRankBy(n)
 {
+<<<<<<< HEAD
 
   var list = document.querySelectorAll('.ranked-list');
+=======
+  var list = document.querySelectorAll("ranked-list")
+>>>>>>> 4990112918dc3e40b7e2a262fa47cbecf7fd5e73
   
   for (let i = 0; i < list.length; i++) 
   {
     var child = list[i].children
+<<<<<<< HEAD
     for (let j = 0; j < child.length; j++) {
       child[j].innerHTML = parseInt(child[j].innerHTML) + n
     }
   }
 }
+=======
+    for (let j = 0; j <  child.length; j++) {
+      child[j].innerHTML = parseInt(child[j].innerHTML) + n
+    }
+  }
+>>>>>>> 4990112918dc3e40b7e2a262fa47cbecf7fd5e73
 
 
 function deepestChild()
 {
+<<<<<<< HEAD
   var node = document.getElementById('grand-node').querySelectorAll('div')
   
     var subNode = 0
@@ -33,4 +45,8 @@ function deepestChild()
     }
   return subNode;
 
+=======
+  
+  
+>>>>>>> 4990112918dc3e40b7e2a262fa47cbecf7fd5e73
 }
